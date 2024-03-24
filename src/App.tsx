6a0c97db9a3cb286f3e39786d95451ae036879dc
allenk <ellenaponomarenko@gmail.com>
2024-03-24 18:31:19 +0300

    add main logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import { cnApp } from './App.classname';
+import { Feed } from './components/Feed';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className={cnApp()}>
+            <h1 className={cnApp("Title")}>8.3 Работа с формами в React</h1>
+
+            <h2 className={cnApp("Subtitle")}>Диванное</h2>
+
+            <Feed />
+        </div>
+    );
 }
 
 export default App;
